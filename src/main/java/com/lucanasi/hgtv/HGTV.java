b33fd6b94f2a471e1d9cf8cd20db2f5ee99b0134
Luca N <lucanasi@MacBook-Pro-di-Luca.local>
2023-08-05 20:14:03 +0200

    Adding item

--- src/main/java/com/lucanasi/hgtv/HGTV.java
+++ src/main/java/com/lucanasi/hgtv/HGTV.java
@@ -1,10 +1,14 @@
 package com.lucanasi.hgtv;
 
+import com.lucanasi.hgtv.item.RegisterItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.IModBusEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -14,8 +18,17 @@ public class HGTV {
 
     public HGTV() { // Constructor della mod
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        RegisterItems.register(eventBus);
+
         MinecraftForge.EVENT_BUS.register(this);
         eventBus.addListener(Common::init);
+    }
+
+    private void addCreative (BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(RegisterItems.CREATIVE_COVER);
+        }
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
--- src/main/java/com/lucanasi/hgtv/item/RegisterItems.java
+++ src/main/java/com/lucanasi/hgtv/item/RegisterItems.java
@@ -1,4 +1,20 @@
 package com.lucanasi.hgtv.item;
 
+import com.lucanasi.hgtv.HGTV;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class RegisterItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HGTV.MOD_ID);
+
+public static final RegistryObject<Item> CREATIVE_COVER = ITEMS.register("creative_cover",
+        () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
